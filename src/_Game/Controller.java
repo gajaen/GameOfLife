d184dd61abd7de4f1e31e958c0a34b9a7536c013
Sivert <s315325@stud.hioa.no>
2017-04-06 19:11:22 +0200

    NADA

--- src/_Game/Controller.java
+++ src/_Game/Controller.java
@@ -37,194 +37,70 @@ public class Controller {
     public Timeline timeline;
     public ColorPicker colorPicker;
 
-    public GUI gui;
-
-
-    public int gen = 0;
-    public int FPS = 120;
-
 
 
     public void cleanBoard() {
-        gc.clearRect(0, 0, CanvasId.getWidth(), CanvasId.getHeight());
-        gc.setFill(backgroundColor);
-        gc.fillRect(0, 0, CanvasId.getWidth(), CanvasId.getHeight());
 
     }
 
 
-    public void initialize() {
+    public void initialize()
+    {
+        GUI gui = new GUI();
+        gui.Canvas();
 
-        gc = CanvasId.getGraphicsContext2D();
-        gc.setFill(backgroundColor);
-        gc.fillRect(0, 0, CanvasId.getWidth(), CanvasId.getHeight());
-
-        //Variabler til spillbrettet
-        cellSize = 10;
-        cellGap = 1;
-        lineWidth = 0.4;
-
-        HEIGHT = ((int) CanvasId.getHeight());
-        WIDTH = ((int) CanvasId.getWidth());
-
-        board = new int[HEIGHT][WIDTH];
-        cleanBoard = new int[HEIGHT][WIDTH];
-
-        System.out.println("CanvasHeight = " + (int) CanvasId.getHeight());
-        System.out.println("CanvasWidth = " + (int) CanvasId.getWidth());
-        System.out.println("Current FPS = " + FPS);
-
-        //FARGER
-        cellColor = Color.ALICEBLUE;
-        lineColor = Color.BLACK;
-        backgroundColor = Color.GREY;
-
-        drawCells();
-        drawLines();
-        //Timeline();
     }
-
 
     public void CanvasPressed(MouseEvent a) {
-
-        int j = ((int) a.getX() / cellSize) + 1;
-        int i = ((int) a.getY() / cellSize) + 1;
-
-        if( j != oldJ || i != oldI ) {
-
-            if (board[i][j] == 0) {
-                board[i][j] = 1;
-            }
-        }
-        oldJ = j;
-        oldI = i;
-
-        drawCells();
-        drawLines();
-
-
     }
 
 
     public void nextGeneration() {
-        cleanBoard();
-        //System.out.println("Generation = " + gen);
-        gen++;
-        int[][] nextBoard = new int[HEIGHT][WIDTH];
 
-        for (int x = 1; x < HEIGHT - 1; x++) {
-            for (int y = 1; y < WIDTH - 1; y++)
-
-            {
-                int cellNeighbors = 0;
-                for (int i = -1; i <= 1; i++) {
-                    for (int j = -1; j <= 1; j++) {
-                        cellNeighbors += board[x + i][y + j];
-                    }
-                }
-
-                cellNeighbors -= board[x][y];
-                if ((board[x][y] == 1) && (cellNeighbors < 2)) nextBoard[x][y] = 0;           // Mindre enn 2 rundt
-                else if ((board[x][y] == 1) && (cellNeighbors > 3))
-                    nextBoard[x][y] = 0;           // Fler enn 3 rundt seg
-                else if ((board[x][y] == 0) && (cellNeighbors == 3))
-                    nextBoard[x][y] = 1;           // Akkurat 3 rundt seg
-                else nextBoard[x][y] = board[x][y];
-            }
-        }
-        board = nextBoard;
-
-        drawCells();
-        drawLines();
-        Timeline();
     }
 
 
     public void drawCells() {
-
-        cleanBoard();
-        gc = CanvasId.getGraphicsContext2D();
-        gc.setFill(cellColor);
-
-        for (int i = 0; i < HEIGHT; i++) {
-            for (int j = 0; j < WIDTH; j++) {
-                if (board[i][j] == 1) {
-                    gc.fillRect(cellSize * j - cellSize, cellSize * i - cellSize, cellSize - cellGap, cellSize - cellGap);}
-            }
-        }
     }
 
     public void drawLines() {
-        gc.setStroke(lineColor);
-        gc.setLineWidth(5);
-        gc.strokeRect(0, 0, CanvasId.getWidth(), CanvasId.getHeight());
-        gc.setLineWidth(lineWidth);
-
-        int a = cellSize;
-        int b = cellSize;
-
-        for (int i = 0; i < HEIGHT; i++) {
-            gc.strokeLine(0, a, CanvasId.getWidth(), a);
-            a += cellSize;
-        }
-        for (int i = 0; i < WIDTH; i++) {
-            gc.strokeLine(b, 0, b, CanvasId.getHeight());
-            b += cellSize;
-        }
     }
 
     public void Timeline() {
-        clickedFpsSlider();
-        TIME = 1000 / 60;
-        timeline = new Timeline(new KeyFrame(Duration.millis(TIME), e -> {
-            nextGeneration();
-            timeline.playFromStart();
-
-        }));
 
     }
 
     public void clickedStartButton(){
-        gui.StartButton();
+
     }
 
     public void clickedClearButton(){
-        gui.ClearButton();
+
     }
 
     public void clickedRandomButton() {
-        System.out.println("1");
-        GUI Test = new GUI();
-        Test.RandomButton();
     }
 
     public void clickedColorPicker(){
-        gui.ColorPicker();
+
 
     }
 
     public void clickedStopButton(){
-        gui.StopButton();
     }
 
     public void clickedFpsSlider(){
-        gui.fpsSlider();
     }
 
     public void clickedCellSlider() {
-        gui.cellSlider();
     }
-
-
-
-    public void init(Stage primaryStage) {
-
-        this.stage = stage;
-
+    public void closeWindow() {
+        Platform.exit();
     }
 
 
     public void openFile() throws IOException {
+        /*
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open GOL Shape");
         fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
@@ -337,12 +213,9 @@ public class Controller {
             e.printStackTrace();
 
         }
-    }
+  */  }
 
 
-    public void closeWindow() {
-        Platform.exit();
-    }
 
 
 }
--- src/_Game/GUI.java
+++ src/_Game/GUI.java
@@ -10,62 +10,50 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Slider;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 
 
-public class GUI {
-
-    public Controller controller;
-    public Stage stage;
 
 
-    public void RandomButton() {
-        //Lager en ny random array for hver gang start er trykket.
-        Controller Con = new Controller();
+public class GUI{
+
+    public Canvas CanvasId;
+    public GraphicsContext gc;
+
+    private Controller con = new Controller();
 
 
-        for (int i = 0; i < Con.HEIGHT; i++) {
-            for (int j = 0; j < Con.WIDTH; j++) {
-                Con.board[i][j] = (int) (Math.random() * 2);
-            }
-        }
 
-        System.out.print("2");
-
-        Con.drawCells();
-        Con.drawLines();
-
-        System.out.print("3");
+    public void Canvas(){
+        System.out.println("Canvas1");
+        gc = CanvasId.getGraphicsContext2D();
+        System.out.println("Canvas2");
+        gc.setFill(Color.BLACK);
+        gc.fillRect(0, 0, 10, 10);
     }
 
 
-    public void ClearButton() {
-        controller.gen = 0;
-        controller.timeline.stop();
-        controller.initialize();
-    }
 
-    public void StartButton() {
-        controller.timeline.play();
-    }
 
-    public void ColorPicker() {
-        Color color = controller.colorPicker.getValue();
-        if (color != null) {
-            controller.cellColor = controller.colorPicker.getValue();
-        }
-    }
 
-    public void StopButton() {
-        controller.timeline.stop();
-    }
 
-    public void fpsSlider() {
-        controller.FPS = (int) controller.sliderFPS.getValue();
-    }
-
-    public void cellSlider() {
-        controller.cellSize = (int) controller.cellSlider.getValue();
-    }
 }
