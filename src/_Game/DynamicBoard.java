9140dcc873db94c07730dbb870e8c86fd0bd5e12
Sivert <s315325@stud.hioa.no>
2017-04-27 18:15:13 +0200

    forbedring

--- src/_Game/DynamicBoard.java
+++ src/_Game/DynamicBoard.java
@@ -11,16 +11,11 @@ public class DynamicBoard {
     private List<List<Integer>> dBoard = new ArrayList<List<Integer>>();
 
 
-
-
     public DynamicBoard(int height1, int width2) {
 
         this.height = height1;
         this.width = width2;
         //byte[][] sBoard = new staticBoard.getBoard();
-
-
-
 
     }
 
@@ -32,30 +27,21 @@ public class DynamicBoard {
         int y1 = 10; //Testtall
 
 
-        for(int i = 0; i < x1; i++) {
+        for (int i = 0; i < x1; i++) {
             List<Integer> inner = new ArrayList<Integer>();
-            for(int j = 0; j < y1; j++)
+            for (int j = 0; j < y1; j++)
                 inner.add(0);
-            getdBoard().add(inner);
+            dBoard.add(inner);
         }
 
-        getdBoard().get(0).set(0, 5); // replace fordi Integer er immutable
+        dBoard.get(0).set(0, 5); // replace fordi Integer er immutable
 
         System.out.println("\nDynamisk 2D tabell:");
-        getdBoard().forEach((l)->System.out.println(l));
+        dBoard.forEach((l) -> System.out.println(l));
 
-        getdBoard().get(1).add(10);
+        dBoard.get(1).add(10);
         System.out.println("\nJagged 2D tabell:");
-        getdBoard().forEach((l)->System.out.println(l));
-    }
-
-
-    public List<List<Integer>> getdBoard() {
-        return dBoard;
-    }
-
-    public void setdBoard(List<List<Integer>> dBoard) {
-        this.dBoard = dBoard;
+        dBoard.forEach((l) -> System.out.println(l));
     }
 }
 
