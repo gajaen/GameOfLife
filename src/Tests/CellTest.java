67dea66ece90c48adf60be6985bb72bafd923b45
Sivert <s315325@stud.hioa.no>
2017-05-04 17:53:58 +0200

    tests

---
+++ src/Tests/CellTest.java
@@ -0,0 +1,28 @@
+import junit.framework.TestCase;
+
+/**
+ * Created by Sivert on 04.05.2017.
+ */
+public class CellTest extends TestCase {
+    public void testGetCellColor() throws Exception {
+
+
+    }
+
+    public void testSetCellColor() throws Exception {
+
+    }
+
+    public void testGetCellSize() throws Exception {
+
+    }
+
+    public void testSetCellSize() throws Exception {
+
+    }
+
+    public void testGetCellGap() throws Exception {
+
+    }
+
+}
