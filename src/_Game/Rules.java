3625457094a2f5f5785c0dd4ccdad2be2d524bd0
Sivert <s315325@stud.hioa.no>
2017-05-05 23:50:51 +0200

    Finish

--- src/_Game/Rules.java
+++ src/_Game/Rules.java
@@ -4,13 +4,27 @@ import javafx.fxml.Initializable;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+/**
+ * The Game Of Life program created for HIOA final project
+ * The Main class starts the project from the main method and loading fxml.
+ *
+ * @version 1.0
+ * @since   2017-01-14
+ */
+
 
 public class Rules implements Initializable {
     @Override
-
+/**
+ * Initialize
+ */
     public void initialize(URL location, ResourceBundle resources) {
     }
 
+    /**
+     * Rules
+     * @throws IOException
+     */
     public Rules() throws IOException {
 
 
